af9d556932eb66da922322895b8566a6527309a6
Carter Lee Woetzel <clwoetzel@gmail.com>
2023-07-14 18:35:09 -0500

    Add files via upload
    Adding in initial scripts

---
+++ counter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import os
+
+## Steps to run this script:
+## FIRST, run swap_pair_index_counter.sh - this pulls all the json files for the current pairs
+## SECOND, run counter.py - this will tell you the indexes and what they should be updated to for every reward campaign
+
+filepath = "./pair_staking_config_jsons/"
+
+def count_index(pair_name_json):
+    data = open(filepath + pair_name_json, "r")
+    counter = 0;
+    for line in data:
+        counter+=1
+    print(f"{pair_name_json :<20}" + " \tcurrent index: " + f"{compute_current_campaign_index(counter) :<5}" + "\t | Update To -> " + new_campaign_index(counter) + " for new rewards campaign.")
+
+## -15 is the standard number of lines of all of the LP reward config file + closing bracket info
+## 11 is the number of lines per campaign index
+def compute_current_campaign_index(total_lines):
+    return str(int((total_lines-15) / 11))
+
+def new_campaign_index(total_lines):
+    return str(int((((total_lines-15) / 11)+1)))
+
+my_path = "/home/astgeorge/SecretCLITooling/pair_staking_config_jsons"
+file_list = []
+
+for (directory, subdirectories, files) in os.walk(my_path):
+    file_list.extend(files)
+
+## run index counter on each file in the subdirectory
+for x in file_list:
+    count_index(x)
+
+
